bcfa5164e934a1df87eb5bdc0a9506f7156eabe4
jarvis <jarvischua8002@gmail.com>
2025-08-21 21:17:21 +0800

    Rename main class, add greeting, add *.class in .gitignore

---
+++ src/main/java/Tony.java
@@ -0,0 +1,8 @@
+public class Tony {
+    public static void main(String[] args) {
+        String border = "=".repeat(80);
+        System.out.println(border);
+        System.out.println("Hello! I'm Tony.\nWhat can I do for you?\n" + border);
+        System.out.println("Bye. Hope to see you again soon!\n" + border);
+    }
+}
