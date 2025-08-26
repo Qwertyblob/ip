37259bd7ebb0a22e357e2a7577e850e62d2e190e
jarvis <jarvischua8002@gmail.com>
2025-08-27 00:44:29 +0800

    Add DateTimeManager class

---
+++ src/main/java/DateTimeManager.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateTimeManager {
+    private static final DateTimeFormatter DATE_ONLY = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private static final DateTimeFormatter DATE_TIME_12H = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mma");
+
+    public static LocalDateTime parse(String input) {
+        try {
+            return LocalDateTime.parse(input.toUpperCase(), DATE_TIME_12H);
+        } catch (DateTimeParseException e) {
+            LocalDate date = LocalDate.parse(input, DATE_ONLY);
+            return date.atStartOfDay();
+        }
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        if (dateTime.toLocalTime().equals(java.time.LocalTime.MIDNIGHT)) {
+            return dateTime.toLocalDate().format(DATE_ONLY);
+        } else {
+            return dateTime.format(DATE_TIME_12H);
+        }
+    }
+}
