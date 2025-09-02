3413fe88540f46ed4eb69788560ab11ff37754f3
jarvis <jarvischua8002@gmail.com>
2025-09-03 02:15:01 +0800

    Update all methods in UI to return String instead of void

--- src/main/java/tony/ui/UI.java
+++ src/main/java/tony/ui/UI.java
@@ -7,93 +7,122 @@ import tony.tasks.Task;
 import tony.tasks.TaskList;
 
 public class UI {
-    private static final String BORDER = "\t" + "=".repeat(80);
     private Scanner sc;
 
     public UI() {
         sc = new Scanner(System.in);
     }
 
-    public void showAddTask(TaskList list, Task task) {
-        System.out.println(BORDER);
-        System.out.println("\tAlright, just let one of my machines do it for you:\n\t  " + task);
-        System.out.println("\tNow you have " + list.getSize() + " tasks in the list.");
-        System.out.println(BORDER);
+    public String showAddTask(TaskList list, Task task) {
+        StringBuilder s = new StringBuilder();
+        //System.out.println(BORDER);
+        //System.out.println("\tAlright, just let one of my machines do it for you:\n\t  " + task);
+        //System.out.println("\tNow you have " + list.getSize() + " tasks in the list.");
+        //System.out.println(BORDER);
+        s.append("Alright, just let one of my machines do it for you:\n  ").append(task);
+        s.append("\nNow you have ").append(list.getSize()).append(" tasks in the list.");
+        return s.toString();
     }
 
-    public void greeting() {
-        System.out.println(BORDER + "\n\tHey, it's Tony.");
-        System.out.println("\tGenius, billionaire, philanthropist… and apparently your personal assistant now.\n"
-                + BORDER);
+    public String greeting() {
+        StringBuilder s = new StringBuilder();
+        //System.out.println(BORDER + "\n\tHey, it's Tony.");
+        //System.out.println("\tGenius, billionaire, philanthropist… and apparently your personal assistant now.\n" + BORDER);
+        s.append("Hey, it's Tony.\n");
+        s.append("Genius, billionaire, philanthropist… and apparently your personal assistant now.");
+        return s.toString();
     }
 
-    public void exit() {
-        System.out.println(BORDER + "\n\tI’m powering down. Don’t break anything while I’m gone.\n" + BORDER);
+    public String exit() {
+        StringBuilder s = new StringBuilder();
+        s.append("I’m powering down. Don’t break anything while I’m gone.");
+        //System.out.println(BORDER + "\n\tI’m powering down. Don’t break anything while I’m gone.\n" + BORDER);
+        return s.toString();
     }
 
-    public String readCommand() {
-        return sc.nextLine();
+//    public String readCommand() {
+//        return sc.nextLine();
+//    }
+
+    public String showError(String msg) {
+        return msg;
     }
 
-    public void showError(String msg) {
-        System.out.println(msg);
-    }
-
-    public void showList(TaskList list) {
-        System.out.println(BORDER);
-        System.out.println("\tJARVIS, show them their list of tasks:");
+    public String showList(TaskList list) {
+        StringBuilder s = new StringBuilder();
+        s.append("JARVIS, show them their list of tasks:\n");
+        //System.out.println(BORDER);
+        //System.out.println("\tJARVIS, show them their list of tasks:");
         for (int i = 1; i <= list.getSize(); i++) {
-            System.out.println("\t" + i + ": " + list.getList().get(i - 1).toString());
+            s.append("  ").append(i).append(": ").append(list.getList().get(i - 1).toString()).append("\n");
         }
-        System.out.println(BORDER);
+        //System.out.println(BORDER);
+        return s.toString();
     }
 
-    public void showMark(Task t) {
+    public String showMark(Task t) {
         t.markDone();
-        System.out.println(BORDER);
-        System.out.println("\tDone. Look at you, being efficient.");
-        System.out.println("\t  " + t);
-        System.out.println(BORDER);
+        //System.out.println(BORDER);
+        //System.out.println("\tDone. Look at you, being efficient.");
+        //System.out.println("\t  " + t);
+        //System.out.println(BORDER);
+        StringBuilder s = new StringBuilder();
+        s.append("Done. Look at you, being efficient.\n  ");
+        s.append(t);
+        return s.toString();
     }
 
-    public void showUnmark(Task t) {
+    public String showUnmark(Task t) {
         t.markUndone();
-        System.out.println(BORDER);
-        System.out.println("\tUnmarked. Happens to the best of us.");
-        System.out.println("\t  " + t);
-        System.out.println(BORDER);
+        //System.out.println(BORDER);
+        //System.out.println("\tUnmarked. Happens to the best of us.");
+        //System.out.println("\t  " + t);
+        //System.out.println(BORDER);
+        StringBuilder s = new StringBuilder();
+        s.append("Unmarked. Happens to the best of us.\n  ");
+        s.append(t);
+        return s.toString();
     }
 
-    public void showDelete(Task t) {
-        System.out.println(BORDER);
-        System.out.println("\tOverachieving might not be for everybody.");
-        System.out.println("\t  " + t);
-        System.out.println(BORDER);
+    public String showDelete(Task t) {
+        //System.out.println(BORDER);
+        //System.out.println("\tOverachieving might not be for everybody.");
+        //System.out.println("\t  " + t);
+        //System.out.println(BORDER);
+        StringBuilder s = new StringBuilder();
+        s.append("Overachieving might not be for everybody.\n  ");
+        s.append(t);
+        return s.toString();
     }
 
-    public void showTasksOnDate(ArrayList<Task> tasks, boolean isFound) {
-        System.out.println(BORDER);
+    public String showTasksOnDate(ArrayList<Task> tasks, boolean isFound) {
+        //System.out.println(BORDER);
+        StringBuilder s = new StringBuilder();
         if (!isFound) {
-            System.out.println("\tYou're as busy as a rock");
+            //System.out.println("\tYou're as busy as a rock");
+            s.append("You're as busy as a rock");
         } else {
             for (Task task : tasks) {
-                System.out.println("\t  " + task);
+                //System.out.println("\t  " + task);
+                s.append("  ").append(task).append("\n");
             }
         }
-        System.out.println(BORDER);
+        //System.out.println(BORDER);
+        return s.toString();
     }
 
-    public void showFound(ArrayList<Task> tasks, String keyword) {
-        System.out.println(BORDER);
+    public String showFound(ArrayList<Task> tasks, String keyword) {
+        StringBuilder s = new StringBuilder();
         if (tasks.isEmpty()) {
-            System.out.println("\tWell, I tried my best.");
+            s.append("Well, I tried my best.");
         } else {
-            System.out.println("\tThat didn't take long.");
+            s.append("That didn't take long.");
             for (int i = 1; i <= tasks.size(); i++) {
-                System.out.println("\t" + i + ": " + tasks.get(i - 1).toString());
+                s.append("\t").append(i).append(": ").append(tasks.get(i - 1).toString()).append("\n");
             }
         }
-        System.out.println(BORDER);
+        //System.out.println(BORDER);
+        return s.toString();
     }
 
 }
