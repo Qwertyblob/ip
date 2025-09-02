6107a973b71b60ce3b34f5222d440df3e3ef96e3
jarvis <jarvischua8002@gmail.com>
2025-09-03 02:10:43 +0800

    Remove border when returning exception

--- src/main/java/tony/exceptions/TonyException.java
+++ src/main/java/tony/exceptions/TonyException.java
@@ -1,9 +1,8 @@
 package tony.exceptions;
 
 public class TonyException extends Exception {
-    private static final String border = "\t" + "+".repeat(80);
 
     public TonyException(String string) {
-        super(border + "\n\t" + string + "\n" + border);
+        super(string);
     }
 }
